2d61de1f70ff670d8b006c7fb24d0935d425b615
mktech <9596910+mktex@users.noreply.github.com>
2023-10-30 11:04:27 +0100

    . update .

--- flaubert/fselekt/feature_importance/feature_importance.py
+++ flaubert/fselekt/feature_importance/feature_importance.py
@@ -92,7 +92,7 @@ class FeatureWichtigkeit():
     def _setup_multikolinearitaet(self):
         # Entferne Multikolinearität
         Xnmk = df_redux.reduktion_multikolinearitaet(self.X, threshold_corr=0.95,
-                                                     target_col=self.zielvariable, id_col=self.id_col)
+                                                     target_col=None, id_col=None)
         # Überschreibt X
         self.X = Xnmk
         # Führe die RFC noch einmal
